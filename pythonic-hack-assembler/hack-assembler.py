b18b4adcfe702bca8add7305ef2dcf5b6ca35b55
Dar Levy <darlevy8@gmail.com>
2022-12-11 21:31:21 +0200

    align indentations and use self method

--- pythonic-hack-assembler/hack-assembler.py
+++ pythonic-hack-assembler/hack-assembler.py
@@ -1,11 +1,12 @@
 import os
 from parser import Parser
 
+
 class HackAssembler:
     def __init__(self, input_file_path):
         self.parser = Parser()
+        self.file_content = self._read_file(input_file_path)
         self.binary_file_content = []
-        self.file_content = _read_file(input_file_path)
         self.output_file_path = os.path.dirname(input_file_path) + '\\output_file.hack'
 
     def assemble(self):
@@ -23,9 +24,9 @@ class HackAssembler:
 
         output_file.close()
 
-   def _read_file(self, file_path):
-        input_file = open(file_path, "r")
-        lines = input_file.readlines()
-        input_file.close()
+    def _read_file(self, file_path):
+        with open(file_path, "r") as input_file:
+            lines = input_file.readlines()
 
+        input_file.close()
         return lines
