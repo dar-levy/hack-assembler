ea4c1a2eb049dcbce7e33cd0009b156cf7e1f6e7
Dar Levy <darlevy8@gmail.com>
2022-12-11 16:54:30 +0200

    Create get_address() method and convert all keys in self.data to char

--- pythonic-hack-assembler/symbol-table.py
+++ pythonic-hack-assembler/symbol-table.py
@@ -2,35 +2,38 @@
 class SymbolTable:
     def __init__(self):
         self.data = {
-            "R0": 0,
-            "R1": 1,
-            "R2": 2,
-            "R3": 3,
-            "R4": 4,
-            "R5": 5,
-            "R6": 6,
-            "R7": 7,
-            "R8": 8,
-            "R9": 9,
-            "R10": 10,
-            "R11": 11,
-            "R12": 12,
-            "R13": 13,
-            "R14": 14,
-            "R15": 15,
-            "SCREEN": 16384,
-            "KBD": 24576,
-            "SP": 0,
-            "LCL": 1,
-            "ARG": 2,
-            "THIS": 3,
-            "THAT": 4
+            'R0': 0,
+            'R1': 1,
+            'R2': 2,
+            'R3': 3,
+            'R4': 4,
+            'R5': 5,
+            'R6': 6,
+            'R7': 7,
+            'R8': 8,
+            'R9': 9,
+            'R10': 10,
+            '11': 11,
+            '12': 12,
+            '13': 13,
+            '14': 14,
+            '15': 15,
+            'SCREEN': 16384,
+            'KBD': 24576,
+            'SP': 0,
+            'LCL': 1,
+            'ARG': 2,
+            'THIS': 3,
+            'THAT': 4
         }
 
     def add_entry(self, symbol, address):
-        self.data[f"{symbol}"] = address
+        self.data[f'{symbol}'] = address
 
     def contains(self, symbol):
         if symbol in self.data:
             return True
         return False
+
+    def get_address(self, symbol):
+        return self.data[f'{symbol}']
