54e928ad7ba3e9f2195a6ddcdd1e9fac17a3ddea
Dar Levy <darlevy8@gmail.com>
2022-12-11 19:55:40 +0200

    Create Constructor

--- pythonic-hack-assembler/hack-assembler.py
+++ pythonic-hack-assembler/hack-assembler.py
@@ -1,4 +1,12 @@
+from parser import Parser
+
 
 class HackAssembler:
-    def __init__(self):
-        self.parser = "Parser" # Construct the future PARSER here
+    def __init__(self, input_file_path, output_file_path):
+        self.parser = Parser()
+        self.input_file_path = input_file_path
+        self.output_file_path = output_file_path
+
+   def _read_file():
+        input_file = open(self.input_file_path, "r")
+
